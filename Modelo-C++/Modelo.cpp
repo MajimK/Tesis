5f944297d44057cf5c5b47b0647c4042d8f926b0
Kevin_Majim <96319834+MajimK@users.noreply.github.com>
2024-11-05 16:06:46 -0500

    feat: Apply criteria

--- Modelo-C++/Modelo.cpp
+++ Modelo-C++/Modelo.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <tuple>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -10,6 +11,8 @@ class Graph
 {
 public:
     int total_cost = 0;
+    int capacity = 0;
+    vector<int> demands;
     vector<int> clients;
     vector<pair<int, int>> bridges;
     vector<vector<pair<int, int>>> routes;
@@ -18,7 +21,7 @@ public:
     map<pair<int, int>, int> c;
     map<tuple<int, int, int>, int> S;
     map<tuple<int, int, int, int>, int> K, L;
-    Graph(const vector<vector<int>> &matrix, const vector<vector<int>> &routes) : weight(matrix)
+    Graph(const vector<vector<int>> &matrix, const vector<vector<int>> &routes /*, const vector<int> clients_demands*/) : weight(matrix) /*, demands(clients_demands)*/
     {
         add_route(routes);
         calc_constant();
@@ -30,6 +33,104 @@ public:
             cout << bridge.first << " -> " << bridge.second << endl;
         int total_cost = 0;
         cout << "Weight: " << total_cost << endl;
+    }
+    void apply_criteria(const tuple<int, int, int, int, int> &key)
+    {
+        int r1 = get<0>(key);
+        int j1 = get<1>(key);
+        int j2 = get<2>(key);
+        int r2 = get<3>(key);
+        int i = get<4>(key);
+
+        vector<vector<pair<int, int>>> routes_criteria;
+        vector<pair<int, int>> saves;
+
+        for (size_t k = 0; k < routes.size(); k++)
+        {
+            vector<pair<int, int>> path;
+            if (k == r1)
+            {
+                int j1first = 0;
+                int j2second = 0;
+                int j1second = 0;
+                int j2first = 0;
+                for (size_t z = 0; z < routes[r1].size(); z++)
+                {
+                    if (z == j1)
+                    {
+                        j1first = routes[r1][z].first;
+                        j1second = routes[r1][z].second;
+                    }
+                    else if (z == j2)
+                    {
+                        j2first = routes[r1][z].first;
+                        j2second = routes[r1][z].second;
+                        path.push_back({j1first, j2second});
+                        if (j1 == j2 - 1)
+                        {
+                            saves.push_back({j1second, j2first});
+                        }
+                    }
+                    else if (z > j1 && z < j2)
+                    {
+                        saves.push_back(routes[r1][z]);
+                    }
+                    else
+                    {
+                        path.push_back(routes[r1][z]);
+                    }
+                }
+            }
+
+            else if (k == r2)
+            {
+                int ifirst = 0;
+                int isecond = 0;
+                for (size_t z = 0; z < routes[r2].size(); z++)
+                {
+                    if (z == i)
+                    {
+                        ifirst = routes[r2][z].first;
+                        isecond = routes[r2][z].second;
+                        for (size_t h = 0; h < saves.size(); h++)
+                        {
+                            if (h == 0)
+                            {
+                                path.push_back({ifirst, saves[h].first});
+                            }
+                            if (h >= 0 && h <= saves.size() - 1)
+                            {
+                                path.push_back(saves[h]);
+                            }
+                            if (h == saves.size() - 1)
+                            {
+                                path.push_back({saves[h].second, isecond});
+                            }
+                        }
+                    }
+                    else
+                    {
+                        path.push_back(routes[r2][z]);
+                    }
+                }
+            }
+            else
+            {
+                for (size_t z = 0; z < routes[k].size(); z++)
+                {
+                    path.push_back(routes[k][z]);
+                }
+            }
+            routes_criteria.push_back(path);
+        }
+
+        for (const auto &route : routes_criteria)
+        {
+            for (const auto &bridge : route)
+            {
+                cout << bridge.first << "->" << bridge.second << endl;
+            }
+        }
     }
 
 private:
@@ -140,45 +241,9 @@ void printear(const map<pair<int, int>, int> &matrix)
         cout << "{" << item.first.first << ", " << item.first.second << "} -> " << item.second << endl;
 }
 
-// int main()
-// {
-//     // Matriz de costos inicial
-//     vector<vector<int>> matrix_cost = {
-//         {0, 5, 4, 5, 1, 4},
-//         {5, 0, 6, 5, 2, 1},
-//         {4, 6, 0, 1, 3, 5},
-//         {5, 5, 1, 0, 6, 7},
-//         {1, 2, 3, 6, 0, 8},
-//         {4, 1, 5, 7, 8, 0}};
+/*
 
-//     // Lista de rutas inicial
-//     vector<vector<int>> list = {
-//         {2, 3, 1},
-//         {4, 5}};
+(0,1) (1,2) (2,3) (3,0)   0->1->2->3->0
+(0,4) (4,5) (5,0)         0->4->5->0
 
-//     // Crear modelo de grafo
-//     Graph model(matrix_cost, list);
-
-//     model.print_graph();
-//     for (auto &item : model.route_cost)
-//     {
-//         cout << "Route " << item.first << " cost: " << item.second << endl;
-//     }
-
-//     // cout << "Matrix C" << endl;
-//     // printear(model.c);
-
-//     // cout << "Matrix S" << endl;
-//     // for (const auto &item : model.S)
-//     //     cout << "{" << get<0>(item.first) << ", " << get<1>(item.first) << ", " << get<2>(item.first) << "} -> " << item.second << endl;
-
-//     // cout << "Matrix K" << endl;
-//     // for (const auto &item : model.K)
-//     //     cout << "{" << get<0>(item.first) << ", " << get<1>(item.first) << ", " << get<2>(item.first) << ", " << get<3>(item.first) << "} -> " << item.second << endl;
-
-//     // cout << "Matrix L" << endl;
-//     // for (const auto &item : model.L)
-//     //     cout << "{" << get<0>(item.first) << ", " << get<1>(item.first) << ", " << get<2>(item.first) << ", " << get<3>(item.first) << "} -> " << item.second << endl;
-
-//     return 0;
-// }
+*/
