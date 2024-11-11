bd18bd59ee992e773f0ab953bf183571000abbc7
Kevin_Majim <96319834+MajimK@users.noreply.github.com>
2024-11-10 23:43:37 -0500

    feat: Create utils for transform language data

---
+++ Modelo python/Utils.py
@@ -0,0 +1,21 @@
+import os
+path = os.path.join(os.getcwd(),"Modelo python")
+path1 = os.path.join(path,"change.txt")
+path2 = os.path.join(path,"change_complete.txt")
+
+with open(path1,'r') as file:
+    list = file.readlines()
+    file.close()
+
+for item in list:
+    item = item.replace("                                       ","")
+    item=item.replace("(","{")
+    item=item.replace(")","},")
+    item=item.replace("\t","")
+    item=item.replace(" ",",")
+    with open(path2,'a') as file:
+        file.write(item)
+
+
+# Chasing cars 
+# snow algo
